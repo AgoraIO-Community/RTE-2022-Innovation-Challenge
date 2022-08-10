3c5faade8da0009437df6f98a6d09ba0dcbbc72d
YajunWang <wangyajun@hetao101.com>
2022-08-10 14:28:52 +0800

    feat: runner macos

--- Application-Challenge/junaya-ΩPaking/omega_paking/lib/pages/chat/index.dart
+++ Application-Challenge/junaya-ΩPaking/omega_paking/lib/pages/chat/index.dart
@@ -1,36 +1,25 @@
-import 'dart:io';
-
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:agora_rtc_engine/rtc_local_view.dart' as rtc_local_view;
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as rtc_remote_view;
+import 'package:omega_paking/config/agora.config.dart' as config;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:agora_rtc_engine/rtc_engine.dart';
-import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
-import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
-import 'package:omega_paking/config/agora.config.dart' as config;
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:provider/provider.dart';
 
-const channel = "aya";
-
+/// MultiChannel Example
 class ChatPage extends StatefulWidget {
+  /// Construct the [ChatPage]
   const ChatPage({Key? key}) : super(key: key);
 
   @override
-  State<ChatPage> createState() => _ChatPageState();
+  State<StatefulWidget> createState() => _State();
 }
 
-class _ChatPageState extends State<ChatPage> {
+class _State extends State<ChatPage> {
   late final RtcEngine _engine;
-  
-  int? _remoteUid;
-  bool _localUserJoined = false;
-  bool _enableAudio = true;
-  bool _enableVideo = true;
-  bool isJoined = false;
-  bool switchCamera = true;
-  bool switchRender = true;
-  List<int> remoteUid = [];
 
+  bool isJoined = false, switchCamera = true, switchRender = true;
+  List<int> remoteUid = [];
   late TextEditingController _controller;
   bool _isRenderSurfaceView = false;
 
@@ -56,7 +45,7 @@ class _ChatPageState extends State<ChatPage> {
     await _engine.setChannelProfile(ChannelProfile.LiveBroadcasting);
     await _engine.setClientRole(ClientRole.Broadcaster);
   }
-  
+
   void _addListeners() {
     _engine.setEventHandler(RtcEngineEventHandler(
       warning: (warningCode) {
@@ -92,12 +81,11 @@ class _ChatPageState extends State<ChatPage> {
       },
     ));
   }
-  
+
   _joinChannel() async {
-    if (Platform.isAndroid && Platform.isIOS) {
+    if (defaultTargetPlatform == TargetPlatform.android) {
       await [Permission.microphone, Permission.camera].request();
     }
-
     await _engine.joinChannel(config.token, _controller.text, null, config.uid);
   }
 
@@ -122,97 +110,78 @@ class _ChatPageState extends State<ChatPage> {
     });
   }
 
-  _toggleEnableAudio() {
-    print('############### _toggleEnableAudio :::');
-    setState(() {
-      _enableAudio = !_enableAudio;
-    });
-    _engine.muteLocalAudioStream(_enableAudio);
-  }
-
-  _toggleEnableVideo() {
-    print('############### _toggleEnableVideo :::');
-    _engine.muteLocalVideoStream(!_enableVideo);
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Provider.value(
-      value: this,
-      child: Scaffold(
-        body: Stack(
+    return Stack(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            _renderVideo(context),
-            _renderControls(context),
+            if (!kIsWeb &&
+                (defaultTargetPlatform == TargetPlatform.android ||
+                    defaultTargetPlatform == TargetPlatform.iOS))
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                      'Rendered by SurfaceView \n(default TextureView): '),
+                  Switch(
+                    value: _isRenderSurfaceView,
+                    onChanged: isJoined
+                        ? null
+                        : (changed) {
+                            setState(() {
+                              _isRenderSurfaceView = changed;
+                            });
+                          },
+                  )
+                ],
+              ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    onPressed: isJoined ? _leaveChannel : _joinChannel,
+                    child: Text('${isJoined ? 'Leave' : 'Join'} channel'),
+                  ),
+                )
+              ],
+            ),
+            _renderVideo(),
           ],
         ),
-      )
-    );
-  }
-
-  /// button group control
-  Widget _renderControls(BuildContext context) {
-    final ButtonStyle style = ElevatedButton.styleFrom(
-      textStyle: const TextStyle(fontSize: 10),
-      padding: const EdgeInsets.all(0),
-      maximumSize: const Size.square(48),
-      minimumSize: const Size.square(48),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(60)),
-    );
-    return Positioned(
-      top: 0,
-      left: 0,
-      bottom: 0,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: _joinChannel,
-            style: style,
-            child: Text("Join"),
-          ),
-          ElevatedButton(
-            onPressed: _leaveChannel,
-            style: style,
-            child: Text("Leave"),
-          ),
-          ElevatedButton(
-            onPressed: _toggleEnableAudio,
-            style: style,
-            child: _enableAudio ? SvgPicture.asset("assets/icons/mic_off.svg", width: 24, height: 24, color: Colors.white): SvgPicture.asset("assets/icons/mic_off.svg", width: 24, height: 24, color: Colors.white),
-          ),
-          ElevatedButton(
-            onPressed: _toggleEnableVideo,
-            style: style,
-            child: SvgPicture.asset(_enableVideo ? "assets/icons/video_camera_on.svg" : "assets/icons/video_camera_off.svg", width: 24, height: 24, color: Colors.white),
-          ),
-          if (Platform.isIOS || Platform.isAndroid)
-            ElevatedButton(
-              onPressed: _switchCamera,
-              style: style,
-              child: SvgPicture.asset("assets/icons/video_switch.svg", width: 24, height: 24, color: Colors.white),
+        if (defaultTargetPlatform == TargetPlatform.android ||
+            defaultTargetPlatform == TargetPlatform.iOS)
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: _switchCamera,
+                  child: Text('Camera ${switchCamera ? 'front' : 'rear'}'),
+                ),
+              ],
             ),
-          ElevatedButton(
-            onPressed: () => Navigator.pop(context),
-            style: style,
-            child: const Text("Pop")
-          ),
-        ],
-      ),
+          )
+      ],
     );
   }
 
-  Widget _renderVideo(BuildContext context) {
+  _renderVideo() {
     return Expanded(
       child: Stack(
         children: [
           Container(
             child: (kIsWeb || _isRenderSurfaceView)
-                ? const RtcLocalView.SurfaceView(
+                ? const rtc_local_view.SurfaceView(
                     zOrderMediaOverlay: true,
                     zOrderOnTop: true,
                   )
-                : const RtcLocalView.TextureView(),
+                : const rtc_local_view.TextureView(),
           ),
           Align(
             alignment: Alignment.topLeft,
@@ -226,11 +195,11 @@ class _ChatPageState extends State<ChatPage> {
                       width: 120,
                       height: 120,
                       child: (kIsWeb || _isRenderSurfaceView)
-                          ? RtcRemoteView.SurfaceView(
+                          ? rtc_remote_view.SurfaceView(
                               uid: e,
                               channelId: _controller.text,
                             )
-                          : RtcRemoteView.TextureView(
+                          : rtc_remote_view.TextureView(
                               uid: e,
                               channelId: _controller.text,
                             ),
@@ -243,20 +212,5 @@ class _ChatPageState extends State<ChatPage> {
         ],
       ),
     );
-  }
-  
-  // Display remote user's video
-  Widget _remoteVideo() {
-    if (_remoteUid != null) {
-      return RtcRemoteView.SurfaceView(
-        uid: _remoteUid!,
-        channelId: channel,
-      );
-    } else {
-      return Text(
-        'Please wait for remote user to join',
-        textAlign: TextAlign.center,
-      );
-    }
   }
 }

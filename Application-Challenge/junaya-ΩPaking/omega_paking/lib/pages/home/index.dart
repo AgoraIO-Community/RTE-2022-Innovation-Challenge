5eca3725526ccdd35b3795cd0a31bc8f7209cb80
YajunWang <wangyajun@hetao101.com>
2022-08-16 16:45:32 +0800

    feat: toggle theme

--- Application-Challenge/junaya-ΩPaking/omega_paking/lib/pages/home/index.dart
+++ Application-Challenge/junaya-ΩPaking/omega_paking/lib/pages/home/index.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:omega_paking/models/app_model.dart';
+import 'package:omega_paking/themes.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,15 +9,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Provider.value(
+      value: this,
+      child: _HomePageStateView(),
+    );
+  }
+}
+
+class _HomePageStateView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    _HomePageState state = context.watch();
+    AppTheme theme = context.watch();
     return Scaffold(
+      backgroundColor: theme.bg2,
       body: Center(
-        child: Column(
-          children: [
-            Text('Home'),
-          ],
-        ),
+        child: Text('Home Page1'),
+      ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<AppModel>().toggleTheme();
+        },
+        child: Icon(Icons.add),
       ),
     );
   }

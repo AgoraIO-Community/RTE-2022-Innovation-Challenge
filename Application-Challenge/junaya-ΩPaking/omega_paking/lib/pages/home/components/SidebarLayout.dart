77706fe2f3a12dd156abc66ee9f0c17d6bf7f8f4
YajunWang <wangyajun@hetao101.com>
2022-08-19 15:59:23 +0800

    feat: add sidebar layout btn

--- Application-Challenge/junaya-ΩPaking/omega_paking/lib/pages/home/components/SidebarLayout.dart
+++ Application-Challenge/junaya-ΩPaking/omega_paking/lib/pages/home/components/SidebarLayout.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:omega_paking/_internal/page_routes.dart';
+import 'package:omega_paking/models/app_model.dart';
+import 'package:omega_paking/models/auth_model.dart';
+import 'package:omega_paking/pages/login/index.dart';
+import 'package:omega_paking/styles.dart';
 import 'package:omega_paking/themes.dart';
 import 'package:provider/provider.dart';
 
@@ -38,40 +43,51 @@ class SideBarLayout extends StatelessWidget {
                 physics: StyledScrollPhysics(),
                 padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 controller: scrollController,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text('Side btn'),
-                      SizedBox(height: 8.0),
-                      Text('Side btn'),
-                      SizedBox(height: 8.0),
-                      Text('Side btn'),
-                      SizedBox(height: 8.0),
-                      Text('Side btn'),
-                      SizedBox(height: 8.0),
-                      Text('Side btn'),
-                      SizedBox(height: 8.0),
-                      Text('Side btn'),
-                      SizedBox(height: 8.0),
-                      Text('Side btn'),
-                      SizedBox(height: 8.0),
-                      Text('Side btn'),
-                      SizedBox(height: 8.0),
-                    ],
-                  )
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text('Side btn'),
+                    SizedBox(height: 8.0),
+                    Text('Side btn'),
+                    SizedBox(height: 8.0),
+                    Text('Side btn'),
+                    SizedBox(height: 8.0),
+                    Text('Side btn'),
+                    SizedBox(height: 8.0),
+                    Text('Side btn'),
+                    SizedBox(height: 8.0),
+                    Text('Side btn'),
+                    SizedBox(height: 8.0),
+                    Text('Side btn'),
+                    SizedBox(height: 8.0),
+                    Text('Side btn'),
+                    SizedBox(height: 8.0),
+                  ],
+                )
               ),
             ),
             Expanded(
               flex: 1,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   ElevatedButton(
                     onPressed: () {},
                     child: Row(
-                      children: [
-                        const Icon(Icons.settings, color: Colors.white,),
-                        const SizedBox(width: 4.0),
-                        const Text('Settings'),
+                      children: const [
+                        Icon(Icons.settings, color: Colors.white,),
+                      ],
+                    )
+                  ),
+                  const SizedBox(height: 8.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      context.read<AuthModel>().reset();
+                      Navigator.push<void>(context, PageRoutes.fade(() => LoginPage(), Durations.slow.inMilliseconds * .001));
+                    },
+                    child: Row(
+                      children: const [
+                        Icon(Icons.logout, color: Colors.white,),
                       ],
                     )
                   ),

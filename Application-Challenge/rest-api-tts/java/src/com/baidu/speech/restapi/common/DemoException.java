13337bd712d4390581c182facc69b44de9673e64
sjhfoae <111549525+sjhfoae@users.noreply.github.com>
2022-08-26 00:43:12 +0800

    提交
    提交

---
+++ Application-Challenge/rest-api-tts/java/src/com/baidu/speech/restapi/common/DemoException.java
@@ -0,0 +1,10 @@
+package com.baidu.speech.restapi.common;
+
+/**
+ * DEMO自带的Exception
+ */
+public class DemoException extends Exception {
+    public DemoException(String message) {
+        super(message);
+    }
+}
